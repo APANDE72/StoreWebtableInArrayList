c7af7dd6c75c99fd426b88ce7fbb1d4eeda10d32
APANDE72 <ankit.pandey5610@gmail.com>
2023-12-23 21:55:45 +0530

    My second commit

--- src/test/java/SeleniumTable.java
+++ src/test/java/SeleniumTable.java
@@ -1,21 +1,28 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumTable {
+	
+	public static Logger log;
 
 	public static void main(String[] args) throws InterruptedException {
 		
-		
+		log= LogManager.getLogger(SeleniumTable.class);
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
+		log.info("Browser window size maximized");
 		driver.get("https://demo.seleniumeasy.com/table-search-filter-demo.html");
+		log.info("Demo Selenium website launched");
 		Thread.sleep(5000);
 		StoreTable(driver);
+		log.info("Browser window closed");
 		driver.close();
 		driver.quit();
 	}
