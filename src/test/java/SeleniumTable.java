96b63e1eaa6c53b4f0e969317eb6a4640bdd1149
APANDE72 <ankit.pandey5610@gmail.com>
2023-12-23 22:14:07 +0530

    My Third Commit

--- src/test/java/SeleniumTable.java
+++ src/test/java/SeleniumTable.java
@@ -8,23 +8,41 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class SeleniumTable {
 	
 	public static Logger log;
 
 	public static void main(String[] args) throws InterruptedException {
 		
+		ExtentReports extent= new ExtentReports();
+		ExtentSparkReporter spark =new ExtentSparkReporter("ExtentReport.html");
+		
+		
+		ExtentTest test = extent.createTest("Launch Browser");
+		test.log(Status.PASS, "user launch browser");
+		test.pass("user lauch website verified");
 		log= LogManager.getLogger(SeleniumTable.class);
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
+		test.pass("User Maximized Window");
 		log.info("Browser window size maximized");
 		driver.get("https://demo.seleniumeasy.com/table-search-filter-demo.html");
+		test.pass("URL Opened");
 		log.info("Demo Selenium website launched");
 		Thread.sleep(5000);
 		StoreTable(driver);
 		log.info("Browser window closed");
+		test.pass("Closed");
+		test.warning("window will be closed");
 		driver.close();
 		driver.quit();
+		extent.attachReporter(spark);
+		extent.flush();
 	}
 		
 	public static void StoreTable(WebDriver driver) {
